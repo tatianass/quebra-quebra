e90786da20d21f4e26c214f846e84b729c3c3bdb
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-06-11 18:04:36 -0300

    colocando ranng the especialistas

---
+++ angular/controller/RankingController.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    /**
+     * Implementing Ranking controller
+     * @author Tatiana Saturno
+     */
+    app.controller("RankingController", [
+        function() {
+            var self = this;
+
+            self.rankingRemuneracao2016 = [
+                { ranking: 1, nome: "JAIRO LUIS BROD", valor: "373944.5" },
+                { ranking: 2, nome: "DEBORAH DA SILVA ACHCAR", valor: "365001.1" },
+                { ranking: 3, nome: "DAVID MIRANDA SILVA ALMEIDA", valor: "360190.9" },
+                { ranking: 4, nome: "GUSTAVO MACHADO PIRES", valor: "357732.9" },
+                { ranking: 5, nome: "TULIO CAMBRAIA", valor: "355043.3" },
+                { ranking: 6, nome: "ADELINO FERNANDES DE OLIVEIRA", valor: "353040.4" },
+                { ranking: 7, nome: "NELIEDJA ROCHA LIMA", valor: "352047.2" },
+                { ranking: 8, nome: "RICARDO ALBERTO VOLPE", valor: "351338.9" },
+                { ranking: 9, nome: "EUGENIO GREGGIANIN", valor: "350446.9" },
+                { ranking: 10, nome: "GENILDA SOARES CORTEZ", valor: "348454.2" }
+            ]
+        }
+    ]);
+})();
