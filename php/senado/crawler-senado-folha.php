936eed1be601c142854d0a77a7dfb8b4c1fc7955
Fagner Lima <contato@fagnerlima.pro.br>
2017-07-15 09:40:21 -0300

    Inicia o crawler para os tipos de folha do Senado

--- php/senado/crawler-senado-folha.php
+++ php/senado/crawler-senado-folha.php
@@ -14,27 +14,36 @@ foreach ($arquivos as $arquivo) {
     $periodo = $matches[1];
     
     $sqlServidor = '';
+    $sqlVinculo = '';
     $sqlCargo = '';
+    $sqlTipoFolha = '';
     $sqlFolha = '';
 
     $file = file($arquivo);
     $atributos = explode(',', $file[0]);
-//    var_dump($atributos);
+//    var_dump($atributos);die;
 
     for ($i = 1; $i < count($file); $i++) {
         $registro = explode(',', $file[$i]);
 
         $idServidor = trim($registro[0]);
-        $nomeServidor = trim(escape($registro[1]));
+        $nomeServidor = trim(strtoupper(escape($registro[1])));
         $anoAdmissaoServidor = trim($registro[4]);
         $sqlServidor .= "REPLACE INTO fagne763_quebraquebra.senado_servidor (id, nome, ano_admissao) "
             . "VALUES ({$idServidor}, '{$nomeServidor}', {$anoAdmissaoServidor});\r\n";
 
-        $cargo = trim(escape($registro[5]));
+        $cargo = trim(strtoupper(escape($registro[5])));
         $sqlCargo .= "INSERT IGNORE INTO fagne763_quebraquebra.senado_cargo (descricao) "
             . "VALUES ('{$cargo}');\r\n";
+        
+        $tipoFolha1 = trim(strtoupper(escape($registro[10])));
+        $tipoFolha2 = trim(strtoupper(escape($registro[30])));
+        $sqlTipoFolha .= "INSERT IGNORE INTO fagne763_quebraquebra.senado_tipo_folha (descricao) "
+            . "VALUES ('{$tipoFolha1}'), ('{$tipoFolha2}');\r\n";
+
     }
 
     file_put_contents("../sql/senado-servidor-{$periodo}.sql", $sqlServidor);
     file_put_contents("../sql/senado-cargo-{$periodo}.sql", $sqlCargo);
+    file_put_contents("../sql/senado-tipo-folha-{$periodo}.sql", $sqlTipoFolha);
 }
