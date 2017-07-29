ff0b37f994d39288d6f3e42bc2ab2f1c89a38f0f
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2017-07-29 20:07:49 -0300

    resolvendo problema com parse python 3

--- camara_deputados/url_encode.py
+++ camara_deputados/url_encode.py
@@ -1,10 +1,10 @@
 # Codificar cada linha da entrada como um parametro URL
 
 import sys
-import urllib.parse
+import six.moves.urllib as urllib 
 
 
 lines = sys.stdin.readlines()
 
 for line in lines:
-    print(urllib.parse.quote(line.strip()))
+    print(urllib.parse.quote_plus(line.strip()))
